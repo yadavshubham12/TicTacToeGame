0e67a221edb0325a4148e0118c629a77d1e6722d
yadavshubham12 <shubhamyadav5151@gmail.com>
2023-01-19 13:31:08 +0530

    ADDED: Print player to choose letter X or O

--- src/main/java/com/blz/tictactoe/TicTacToeGame.java
+++ src/main/java/com/blz/tictactoe/TicTacToeGame.java
@@ -1,7 +1,11 @@
 package com.blz.tictactoe;
 
+import java.util.Scanner;
+
 public class TicTacToeGame {
 
+    private char playerLetter;
+    private char computerLetter;
     private char[] board = new char[10];
 
     public TicTacToeGame() {
@@ -9,8 +13,28 @@ public class TicTacToeGame {
             board[i] = ' ';
         }
     }
+    public void chooseLetter() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println(" Enter your letter X or O ");
+        char letter = scanner.next().charAt(0);
+        while (letter != 'X' && letter != 'O'){
+            System.out.println("Invalid input");
+            letter = scanner.next().charAt(0);
+        }
+        playerLetter = letter;
+        computerLetter = (letter == 'X') ? 'O' : 'X';
+    }
     public  void move (int position, char player) {
-        board[position] = player;
+        if (player == playerLetter){
+            board[position] = playerLetter;
+        }
+        else if (player == computerLetter){
+            board[position] = computerLetter;
+        }
+        else {
+            System.out.println("Invalid move");
+        }
+
     }
     public boolean draw() {
         for (int i = 1; i < board.length; i++){
@@ -49,13 +73,15 @@ public class TicTacToeGame {
 
     public static void main(String[] args) {
         TicTacToeGame  game = new TicTacToeGame();
+        game.chooseLetter();
         game.move(1, 'X');
         game.move(5, '0');
         game.move(9, 'X');
-        if (game.win('X')){
-            System.out.println("X wins");
-        } else if (game.win('O')) {
-            System.out.println("O loss");
+        game.printBoard();
+        if (game.win(game.playerLetter)){
+            System.out.println("You win");
+        } else if (game.win(game.computerLetter)) {
+            System.out.println("Computer loss");
         }
         else if (game.draw()){
             System.out.println("Draw The Game");
